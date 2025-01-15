a1aa3d11e5e12af35a81014bd5a6516adb63d8b8
abduN2 <gamingmoments555@gmail.com>
2025-01-14 18:10:17 -0700

    volume slider implementation

--- aghani/PlayerViewController.swift
+++ aghani/PlayerViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Abdulrahman Negmeldin on 2025-01-14.
 //
 
+import AVFoundation
 import UIKit
 
 class PlayerViewController: UIViewController {
@@ -14,6 +15,36 @@ class PlayerViewController: UIViewController {
     
     @IBOutlet var holder: UIView!
     
+    var player: AVAudioPlayer?
+    
+    //UI elements
+    private let albumImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let songNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 //line wrap
+        return label
+    }()
+    
+    private let artistNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 //line wrap
+        return label
+    }()
+    
+    private let albumNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0 //line wrap
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,7 +53,7 @@ class PlayerViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        if holder.subviews.count == 1 {
+        if holder.subviews.count == 0 {
             configure()
         }
     }
@@ -30,8 +61,95 @@ class PlayerViewController: UIViewController {
     func configure() {
         //setup player
         
+        let song = songs[position]
         
+        let urlString = Bundle.main.path(forResource: song.trackName, ofType: "mp3")
+        
+        do {
+            try AVAudioSession.sharedInstance().setMode(.default)
+            try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+            
+            guard let urlString = urlString else {
+                return
+            }
+            
+            player = try AVAudioPlayer(contentsOf: URL(string: urlString)!)
+            
+            guard let player = player else {
+                return
+            }
+            player.volume = 0.5
+            
+            
+            player.play()
+        }
+        catch {
+            print("error occurred")
+        }
         //setup user interface elements
+        
+        //album cover
+        albumImageView.frame = CGRect(x: 10,
+                                      y: 10,
+                                      width: holder.frame.size.width-20,
+                                      height: holder.frame.size.width-20)
+        
+        albumImageView.image = UIImage(named: song.imageName)
+        holder.addSubview(albumImageView)
+        
+        //labels: song name, album, artist
+        songNameLabel.frame = CGRect(x: 10,
+                                     y: albumImageView.frame.size.height+10,
+                                      width: holder.frame.size.width-20,
+                                      height: 70)
+        
+        albumNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height+10+70,
+                                      width: holder.frame.size.width-20,
+                                      height: 70)
+        
+        artistNameLabel.frame = CGRect(x: 10,
+                                      y: albumImageView.frame.size.height+10+140,
+                                      width: holder.frame.size.width-20,
+                                      height: 70)
+        
+        songNameLabel.text = song.name
+        albumNameLabel.text = song.albumName
+        artistNameLabel.text = song.artistName
+        
+        holder.addSubview(songNameLabel)
+        holder.addSubview(albumNameLabel)
+        holder.addSubview(artistNameLabel)
+        
+        //player controls
+        
+        
+        
+        //slider for volume
+        let slider = UISlider(frame: CGRect(x: 20,
+                                            y: holder.frame.size.height-60,
+                                            width: holder.frame.size.width-40,
+                                            height: 50))
+        
+        slider.value = 0.5
+        slider.addTarget(self, action: #selector(didSlideSlider(_:)), for: .valueChanged)
+        holder.addSubview(slider)
+        
+        
+    }
+    
+    @objc func didSlideSlider(_ slider: UISlider) {
+        let value = slider.value
+        player?.volume = value
+        //adjust player volume
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if let player = player {
+            player.stop()
+        }
     }
 
 }
